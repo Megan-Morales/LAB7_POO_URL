6d3a138857b8e768fdf5053f6333f9c9f6a8e01e
Megan-Morales <63917946+Megan-Morales@users.noreply.github.com>
2021-11-09 10:09:04 -0600

    h

--- MyForm.h
+++ MyForm.h
@@ -55,10 +55,10 @@ namespace Lab7 {
 		void InitializeComponent(void)
 		{
 			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
-			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
 			this->groupBox1->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -69,28 +69,17 @@ namespace Lab7 {
 			this->groupBox1->Controls->Add(this->button1);
 			this->groupBox1->Location = System::Drawing::Point(12, 12);
 			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->Size = System::Drawing::Size(573, 588);
+			this->groupBox1->Size = System::Drawing::Size(573, 642);
 			this->groupBox1->TabIndex = 0;
 			this->groupBox1->TabStop = false;
 			this->groupBox1->Text = L"Ejercicio 1";
 			// 
-			// groupBox2
+			// textBox1
 			// 
-			this->groupBox2->Location = System::Drawing::Point(610, 12);
-			this->groupBox2->Name = L"groupBox2";
-			this->groupBox2->Size = System::Drawing::Size(573, 588);
-			this->groupBox2->TabIndex = 1;
-			this->groupBox2->TabStop = false;
-			this->groupBox2->Text = L"Ejercicio 2";
-			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(424, 45);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 31);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"Aceptar";
-			this->button1->UseVisualStyleBackColor = true;
+			this->textBox1->Location = System::Drawing::Point(253, 49);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(136, 22);
+			this->textBox1->TabIndex = 2;
 			// 
 			// label1
 			// 
@@ -101,18 +90,29 @@ namespace Lab7 {
 			this->label1->TabIndex = 1;
 			this->label1->Text = L"Ingrese el tamaño del arreglo: ";
 			// 
-			// textBox1
+			// button1
 			// 
-			this->textBox1->Location = System::Drawing::Point(253, 49);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(136, 22);
-			this->textBox1->TabIndex = 2;
+			this->button1->Location = System::Drawing::Point(424, 45);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 31);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Aceptar";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// groupBox2
+			// 
+			this->groupBox2->Location = System::Drawing::Point(610, 12);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Size = System::Drawing::Size(573, 642);
+			this->groupBox2->TabIndex = 1;
+			this->groupBox2->TabStop = false;
+			this->groupBox2->Text = L"Ejercicio 2";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1195, 626);
+			this->ClientSize = System::Drawing::Size(1195, 666);
 			this->Controls->Add(this->groupBox2);
 			this->Controls->Add(this->groupBox1);
 			this->Name = L"MyForm";
